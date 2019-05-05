c213adfb8105fcf78a20928f7473c55518b013fa
kallavis <50292386+kallavis@users.noreply.github.com>
2019-05-05 19:27:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,122 @@
 #include <iostream>
-#include <string>
+#inlcude <string>
+#include <ctime> //Anlık saati cekmek icin kullanilan kutuphane,saat ve servis bilgileri uzerınde ıslem yapicaz//
 using namespace std;
+bool kgiris;
 
-class Kuriye
+Class kurye
 {
 private:
-    string name;
-    string surname;
+    string ad;
+    string soyad;
     string TC;
-    int age;
-public:
-void setName(string x){name = x;}
-void setSurname(string x){surname = x;}
-void setTC(string x){TC = x;}
-void setAge(int x){age = x;}
-
+    int yas;
 };
 
-class Islem
+Class islem1
 {
 public:
-    void find() {}
-    void list() {}
-    void update() {}
-    void del() {}
-    void add() {}
+    void ekle() {}
+    void listele() {}
+    void ara() {}
+    
 };
+class islem2 {
+    public:
+   void sil() {}
+   void guncelle() {}
+    
+    
+};
+class kullaniciadi_sifre {
+    protected:
+    string kullaniciadi;
+    string sifre;
+};
+class anaclass:public kurye,public islem1,public islem2,public kullaniciadi_sifre {
+    public:
+	void secim();//kullaniciya en basta secim yaptırıcaz servis kaydı ekle çıkış gibi//
+    void giris();//yonetim giris paneli olacak, asagıda  yazdım bakabilirsin kanka//
+	void listele_guncelle();
+	void serviskayidi_ekleme();
+	void serviskayidi_silme();
+	void set_kullaniciadi(string *adres);//kullanici adini setle gonderdim yazdım asagıda da//
+	void set_sifre(string *adres);//sifreyi setle gonderdim yazdım asagıda da//
+	string get_kullaniciadi();//kullanici adini getle cagirdim yazdım asagıda da//
+	string get_sifre();//sifreyi getle cagırdım yazdım asagıda da//
+	anaclass() {
+		kullaniciadi = "oleksi";
+		sifre = "kadir";
+	}
+	
 
-class Mainclass: public Kuriye, public Islem
-{
 };
+void yazdir(string deger, bool durum) {
+	for (int i = 0; i < deger.length(); i++)
+	{
+		cout << deger[i];
+		
+
+	}if (durum)
+	{
+		cout << endl;
+	}
+}   
+string sdonusturme(char *deger) {
+	string deger2;
+	for (int i = 0; i < strlen(deger); i++) {
+		deger2 += deger[i];
+	}
+	return deger2;
+}
+    
+    
+    
+    
+string anaclass::get_kullaniciadi() {
+	return kullaniciadi;
+}
+string anaclass::get_sifre() {
+	return sifre;
+}
+void anaclass::set_kullaniciadi(string *adres) {
+	kullanici = *adres;
+}
+void anaclass::set_sifre(string *adres) {
+	sifre = *adres;
+}
+void anaclass::giris() {
+	char kullaniciadi[50], sifre[50];
+	string kullanici = "", parola = "";
+	yazdir("Kullanici Adi:", true);
+	gets_s(kullaniciadi);
+	yazdir("Sifre", true);
+	gets_s(sifre);
+	for (int i = 0; i < strlen(kullaniciadi); i++)
+	{
+		kullanici += kullaniciadi[i];
+	}
+	for (int i = 0; i < strlen(sifre); i++)
+	{
+		parola += sifre[i];
+	}
+	if (get_kullaniciadi() == kullanici && get_sifre() == parola)
+	{
+		bgiris = true;
+		secim();
+	}
+	else
+	{
+		kgiris = false;
+		yazdir("Hatali giris yaptiniz,lutfen yeniden deneyiniz.", true);
+		giris();
+	}
+}
 
 int main(void)
 {
 
     return 0;
 }
+
+return 0;
